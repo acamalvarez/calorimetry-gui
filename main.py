f2ab1ba79e05628c3678e7091f83cbae7124a4eb
Camilo <acalvm@gmail.com>
2023-10-18 16:59:45 -0500

    Replace buttons by combobox

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import sys
 
 from PyQt6.QtWidgets import (
     QApplication,
+    QComboBox,
     QHBoxLayout,
-    QPushButton,
     QVBoxLayout,
     QStackedWidget,
     QWidget,
@@ -20,35 +20,29 @@ class MainWindow(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.stacked_widget = QStackedWidget(self)
+        self.combo_box = QComboBox()
+        self.combo_box.addItem("Calculate Cp")
+        self.combo_box.addItem("Calculate Tf")
+        self.combo_box.activated.connect(self.show_widget_calculator)
+
+        self.combo_box_layout = QHBoxLayout()
+        self.combo_box_layout.addWidget(self.combo_box)
 
         self.cp_from_experiment_app = CpFromExperimentApp()
         self.tf_from_experiment_app = TfFromCpExperimentApp()
 
+        self.stacked_widget = QStackedWidget(self)
         self.stacked_widget.addWidget(self.cp_from_experiment_app)
         self.stacked_widget.addWidget(self.tf_from_experiment_app)
 
-        self.show_calculate_cp_button = QPushButton("Calculate Cp", self)
-        self.show_calculate_tf_button = QPushButton("Calculate Tf", self)
-
-        self.show_calculate_cp_button.clicked.connect(self.show_calculate_cp_app)
-        self.show_calculate_tf_button.clicked.connect(self.show_calculate_tf_app)
-
-        button_layout = QHBoxLayout()
-        button_layout.addWidget(self.show_calculate_cp_button)
-        button_layout.addWidget(self.show_calculate_tf_button)
-
         main_layout = QVBoxLayout()
+        main_layout.addLayout(self.combo_box_layout)
         main_layout.addWidget(self.stacked_widget)
-        main_layout.addLayout(button_layout)
 
         self.setLayout(main_layout)
 
-    def show_calculate_cp_app(self):
-        self.stacked_widget.setCurrentIndex(0)
-
-    def show_calculate_tf_app(self):
-        self.stacked_widget.setCurrentIndex(1)
+    def show_widget_calculator(self):
+        self.stacked_widget.setCurrentIndex(self.combo_box.currentIndex())
 
 
 app = QApplication(sys.argv)
